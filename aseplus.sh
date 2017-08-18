1f5e737f5c57159dbc72d7bd20485bc0c765cb38
Darius <dariusworks@users.noreply.github.com>
2017-08-18 18:57:41 +0300

    work in progress

---
+++ aseplus.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Title: asemica-plus
+#Description: To be Easier to use. Uses original code of asemica
+#Author: bads.tm
+#Date: 2017-08-18 WIP
+#Usage: aseplus.sh
+
+engine128appstart () {
+#Ask What you want to do
+echo "1. Decrypt a message"
+echo "2. Encrypt a message"
+echo "1a. Decrypt a message from text"
+echo "2a. Encrypt a message from text"
+echo "1ab. Decrypt a message from text output to text"
+echo "2ab. Encrypt a message from text output to text"
+
+read -rn1 wut2do
+
+  if [[ $wut2do == "1" ]] || [[ $wut2do == "1" ]]; then
+    #Decrypt
+    
+  fi
+ 
+ 
+ 
+ 
+ }
+ #does not leave, only something like CTRL+SHIFT+C can stop it
+ engine128appstart
