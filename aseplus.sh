1808be7f7c88cc693926f21b4c4ab3475bb6b8b1
Darius <dariusworks@users.noreply.github.com>
2017-08-20 11:08:45 +0300

    First Portion

--- aseplus.sh
+++ aseplus.sh
@@ -4,20 +4,60 @@
 #Author: bads.tm
 #Date: 2017-08-18 WIP
 #Usage: aseplus.sh
-
+		printf '\033\143'
 engine128appstart () {
 #Ask What you want to do
-echo "1. Decrypt a message"
-echo "2. Encrypt a message"
-echo "1a. Decrypt a message from text"
-echo "2a. Encrypt a message from text"
-echo "1ab. Decrypt a message from text output to text"
-echo "2ab. Encrypt a message from text output to text"
-
+echo "asemica plus - easier to use, made by $(tput setaf 6)$(tput bold)bads.tm$(tput sgr 0)"
+echo " "
+echo "1.  Decrypt a message"
+echo "2.  Encrypt a message"
+echo Not working:
+echo "3.  Decrypt a message from text"
+echo "4.  Encrypt a message from text"
+echo "5 . Decrypt a message from text output to text"
+echo "6.  Encrypt a message from text output to text"
+echo "7.  Decrypt a message from text output to text which is protected"
+echo "8.  Encrypt a message from text output to text which is protected"
+echo "$(tput setaf 1)10. Test Zone$(tput sgr 0)"
+echo " "
+echo "Enter a number !"
 read -rn1 wut2do
 
   if [[ $wut2do == "1" ]] || [[ $wut2do == "1" ]]; then
-    #Decrypt
+  #cls
+  printf '\033\143'
+  		echo "To decrypt message Enter/paste message now"
+		read msg
+		#cls
+		printf '\033\143'
+		echo "Enter/paste text base (-c)"
+		read thec
+				#cls
+		printf '\033\143'
+		echo Please Wait... It is now processing
+		printf '\033\143'
+	theoutput=$(echo "$msg" | ./asemica enc -c $thec)
+	echo "$theoutput"
+	notify-send "$theoutput"
+    
+  fi
+  
+  if [[ $wut2do == "2" ]] || [[ $wut2do == "2" ]]; then
+  #cls
+  printf '\033\143'
+  		echo "To encrypt message Enter/paste message now"
+		read msg
+		#cls
+		printf '\033\143'
+		echo "Enter/paste text base (-c)"
+		read thec
+				#cls
+		printf '\033\143'
+		echo Please Wait... It is now processing
+		printf '\033\143'
+	theoutput=$(echo "$msg" | ./asemica enc -c $thec)
+	echo "$theoutput"
+	notify-send "$theoutput"
     
   fi
  
